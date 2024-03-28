9fc154d75bc37d8951cc31f4b92ec5feba4245da
Asajco <petergacj@gmail.com>
2024-03-28 17:19:23 +0100

    production commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex, Text } from '@chakra-ui/react'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 import { CartContextProvider } from './store/cartContext'
 
+// Set this variable to true if it's a production environment
+const isProduction = true
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
-  // <React.StrictMode>
   <ChakraProvider>
     <CartContextProvider>
       <HashRouter>
-        <App />
+        {!isProduction ? (
+          <App />
+        ) : (
+          <Flex justify="center" align="center" h="100vh">
+            <Text fontSize="2xl" fontWeight="bold">
+              Na stránke pracujeme aby sme Vám priniesli ten najlepší zážitok.
+            </Text>
+          </Flex>
+        )}
       </HashRouter>
     </CartContextProvider>
   </ChakraProvider>,
-  // </React.StrictMode>,
 )
